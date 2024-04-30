2a30de86ceb78d6fd78b607f1d8f229b5ce8e694
EmilianoIrace <39857581+EmilianoIrace@users.noreply.github.com>
2024-04-30 16:01:39 +0200

    Added python connection

---
+++ Interface/Interface.py
@@ -0,0 +1,130 @@
+import serial
+import time
+from icecream import ic
+
+def write_read(x): 
+    ser.write(bytes(x, 'utf-8')) 
+    time.sleep(0.05) 
+    data = ser.readline() 
+    return data 
+
+def read_string():
+    while(1):
+        if(ser.in_waiting>0):
+            received_data = ""
+            while True:
+                char = ser.read().decode()
+                if char == '\n':
+                    break
+                received_data += char.strip()
+            return received_data
+        
+
+def read_int():
+    while(1):
+        if(ser.in_waiting>0):
+            received_data = ser.read().decode().strip()
+            if received_data.isdigit() :
+                return received_data
+            # else:
+            #     ser.write(1)
+        else:
+            time.sleep(0.0001)
+
+# Define the serial port and baud rate
+serial_port = 'COM8'  # Replace with the appropriate port name
+baud_rate = 115200  # Replace with the appropriate baud rate
+
+# Create a serial object
+ser = serial.Serial(serial_port, baud_rate)
+if ser.is_open:
+    ser.close()
+# # Open the serial port
+ser.open()
+
+# Check if the serial port is open
+if ser.is_open:
+    print(f"Serial port {serial_port} is open.")
+    received_data = ""
+    ser.write(bytes("9", 'utf-8'))
+    while( True):
+        
+        if(ser.in_waiting>0):
+            received_data = read_int()
+            # print(f"Received data: {received_data}")
+        else:
+            ser.write(bytes("9", 'utf-8'))
+        time.sleep(0.1)
+        if received_data == "9":
+            received_data = read_int()
+            if received_data == "1":
+                received_data = read_int()
+                if received_data == "3":
+                    ser.write(bytes("1", 'utf-8'))
+                    ser.flush()
+                    break
+    print("Connection established")
+    
+else:
+    print(f"Failed to open serial port {serial_port}.")
+time.sleep(0.02)
+
+if(ser.in_waiting>0):
+    received_data = read_int()
+
+duty = input("Enter the duty cycle: ")
+
+if int(duty)>=int("100"):
+    print("Invalid input, too many pulses,limited to 95")
+    duty="95"
+if int(duty)< int("10"):
+    duty = "0"+duty
+dutyCycle=0
+
+while (True):
+    
+    time.sleep(0.02)
+    ser.write(bytes(duty[0], 'utf-8'))
+    dutyCycle = int(read_int())*10
+    time.sleep(0.02)
+
+    ser.write(bytes(duty[1], 'utf-8'))
+    time.sleep(0.02)
+
+    dutyCycle += int(read_int())
+    
+    time.sleep(0.02)
+
+    if(int(duty) != dutyCycle):
+        ser.write(bytes("5", 'utf-8'))
+        ser.write(bytes("2", 'utf-8'))
+        ser.write(bytes("5", 'utf-8'))
+    else:
+        break
+pulses= str(input("Enter the number of pulses: "))
+
+if int(pulses)>=int("100"):
+    print("Invalid input, too many pulses,limited to 99")
+    pulses="99"
+if int(pulses)<int("10"):
+    pulses = "0"+pulses
+N_pulses=0
+while (True):
+    ser.write(bytes(pulses[1], 'utf-8'))
+    N_pulses = int(read_int())*10
+    time.sleep(0.02)
+
+    ser.write(bytes(pulses[0], 'utf-8'))
+    time.sleep(0.02)
+
+    N_pulses += int(read_int())
+    time.sleep(0.02)
+
+    if(int(pulses) == N_pulses):
+        ser.write(bytes("5", 'utf-8'))
+        ser.write(bytes("2", 'utf-8'))
+        ser.write(bytes("5", 'utf-8'))
+        break
+
+print(f"Duty is: {dutyCycle}")
+print(f"Pulses are: {N_pulses}")
--- src/main.cpp
+++ src/main.cpp
@@ -3,23 +3,34 @@
 
 const int outputPin = PA0;       // Define the output pin
 const int LED = PA5;       // Define the output pin
+const int Solenoid = PA8;
 const int frequency = 20E3;    // Period of the pulse in microseconds (1 second)
 int dutyCycle = 20;      // Duty cycle in percentage (50% duty cycle)
-int numPulses = 10;      // Number of pulses to generate
-
+int pulses = 10;      // Number of pulses to generate
+int x=0;
+char communication_p[]="Connection_pending ";
+char communication_e[]="Connection_established";
+char comm_value = '0';
+int dutyCycle_1 = 0;
+int dutyCycle_2 = 0;
+int pulses_1 = 0;
+int pulses_2 = 0;
 void SettingPWM(void);
 
 void setup() {
   int period = 1E6 / frequency; // Calculate the period of the pulse in microseconds
   pinMode(outputPin, OUTPUT);  // Set the output pin as OUTPUT
+  pinMode(Solenoid, OUTPUT);  // Set the output pin as OUTPUT
   pinMode(LED, OUTPUT);  // Set the output pin as OUTPUT
-  Serial.begin(9600);          // Initialize the serial port
+  Serial.begin(115200);          // Initialize the serial port
 }
 
 void loop() {
   int period = 1E6 / frequency; // Calculate the period of the pulse in microseconds
   SettingPWM();
-  for (int i = 0; i < numPulses; i++) {
+  digitalWrite(Solenoid, LOW); // Turn the output pin ON
+  delay(10);
+  for (int i = 0; i < pulses; i++) {
     // Calculate the ON time and OFF time based on duty cycle and period
     int onTime = (period * dutyCycle) / 100;
     int offTime = period - onTime;
@@ -30,6 +41,10 @@ void loop() {
     digitalWrite(outputPin, LOW);  // Turn the output pin OFF
     delayMicroseconds(offTime);     // Wait for the OFF time
   }
+  digitalWrite(Solenoid, HIGH); // Turn the output pin ON
+  delay(800);
+  digitalWrite(Solenoid, LOW); // Turn the output pin ON
+  delay(800);
   
   // Stop generating pulses after numPulses
   Serial.print("Done");
@@ -44,26 +59,98 @@ void loop() {
 }
 
 void SettingPWM(void)   {
+  // Serial.setTimeout(100);
   int period = 1E6 / frequency; // Calculate the period of the pulse in microseconds
-  Serial.print("Duty cycle: ");
-  Serial.flush();
-  while (!Serial.available()) {
-    dutyCycle = Serial.parseInt();
+  x=0;
+  while (comm_value != '9') {
+    if(Serial.available()>0){
+      comm_value=Serial.read();
+    }
   }
-  Serial.print(dutyCycle);
-  Serial.print("%");
-  Serial.print("\n");
-  Serial.print("Period: ");
-  Serial.print(period);
-  Serial.print(" microseconds");
-  Serial.print("\n");
-  Serial.print("Number of pulses: ");
-  Serial.parseInt();
-  delay(100);
-  while (!Serial.available()) {
-    numPulses = Serial.parseInt();
+
+  while(comm_value!='1'){
+
+    Serial.write('9');
+    Serial.write('1');
+    Serial.write('3');
+    while(!Serial.available());
+    comm_value=Serial.read();
+    delay(10);
   }
-  Serial.print(numPulses);
-  Serial.print(" pulses");
-  Serial.print("\n");
+  dutyCycle=0;
+  delay(40);
+  while(true){
+    while(!Serial.available());
+    dutyCycle_1=Serial.read();
+    delay(10);
+    while(Serial.available()){
+      Serial.read();
+    }
+    Serial.write(dutyCycle_1);
+    delay(10);
+
+    while(!Serial.available());
+    dutyCycle_2=Serial.read();
+    delay(10);
+
+    Serial.write(dutyCycle_2);
+    delay(10);
+    dutyCycle = (dutyCycle_1-'0')*10 + dutyCycle_2-'0';
+    comm_value=Serial.read();
+    if (comm_value=='5'){
+      comm_value=Serial.read();
+      if(comm_value=='2'){
+        comm_value=Serial.read();
+        if (comm_value=='3')
+          break;
+    }
+  }
+  while (true) {
+  pulses=0;
+  while(!Serial.available());
+  pulses_1=Serial.read();
+  delay(10);
+  while(Serial.available()){
+    Serial.read();
+  }
+  Serial.write(pulses_1);
+  delay(10);
+
+  while(!Serial.available());
+  pulses_2=Serial.read();
+  delay(10);
+
+  Serial.write(pulses_2);
+  delay(10);
+  pulses = (pulses_1-'0')*10 + pulses_2-'0';
+
+  comm_value=Serial.read();
+    if (comm_value=='5'){
+      comm_value=Serial.read();
+      if(comm_value=='2'){
+        comm_value=Serial.read();
+        if (comm_value=='3')
+          break;
+    }
+    }
+  }
+  }
+  
+  // Serial.print(dutyCycle);
+  // Serial.print("%");
+  // Serial.print("\n");
+  // Serial.print("Period: ");
+  // Serial.print(period);
+  // Serial.print(" microseconds");
+  // Serial.print("\n");
+  // Serial.print("Number of pulses: ");
+  // Serial.print("\n");
+  // Serial.parseInt();
+  // delay(10);
+  // while (!Serial.available()) {
+  //   numPulses = Serial.parseInt();
+  // }
+  // Serial.print(numPulses);
+  // Serial.print(" pulses");
+  // Serial.print("\n");
 }
