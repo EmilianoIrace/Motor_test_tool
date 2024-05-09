bf2bb454e15f9005b78f2488a3e18a6ee04f505f
EmilianoIrace <39857581+EmilianoIrace@users.noreply.github.com>
2024-05-10 00:40:33 +0200

    Added new version, directly flashable on the unit

--- src/main.cpp
+++ src/main.cpp
@@ -1,120 +1,139 @@
 #include <Arduino.h>
 
+// const uint32_t g_ADigitalPinMap[] = {
+//   // D0 - D7
+//   11,
+//   12,
+//   13,
+//   14,
+//   15,
+//   16,
+//   17,
+//   18,
 
-const int outputPin = PA0;       // Define the output pin
-const int LED = PA5;       // Define the output pin
-const int Solenoid = PA8;
-const int frequency = 20E3;    // Period of the pulse in microseconds (1 second)
-int dutyCycle = 20;      // Duty cycle in percentage (50% duty cycle)
-int pulses = 10;      // Number of pulses to generate
-int x=0;
-char communication_p[]="Connection_pending ";
-char communication_e[]="Connection_established";
-char comm_value = '0';
-int dutyCycle_1 = 0;
-int dutyCycle_2 = 0;
-int pulses_1 = 0;
-int pulses_2 = 0;
-void SettingPWM(void);
+//   // D8 - D13
+//   19,
+//   20,
+//   22,
+//   23,
+//   24,
+//   25,
+
+//   // A0 - A7
+//   3,
+//   4,
+//   28,
+//   29,
+//   30,
+//   31,
+//   5, // AIN3 (P0.05)
+//   2, // AIN0 (P0.02) / AREF
+
+//   // SDA, SCL
+//   26,
+//   27,
+
+//   // RX, TX
+//   8,
+//   6
+// };
+
+
+#define MOTOR_PWM 21
+#define SOL_ON_EN 24
+#define SOL_DEG_EN 9
+#define SOL_DEG_PWM 8
+#define SOL_ON_PWM 20
+#define MOTOR_UI 4
+
+
+// put function declarations here:
+int myFunction(int, int);
+
 
 void setup() {
-  int period = 1E6 / frequency; // Calculate the period of the pulse in microseconds
-  pinMode(outputPin, OUTPUT);  // Set the output pin as OUTPUT
-  pinMode(Solenoid, OUTPUT);  // Set the output pin as OUTPUT
-  pinMode(LED, OUTPUT);  // Set the output pin as OUTPUT
-  Serial.begin(115200);          // Initialize the serial port
+  // put your setup code here, to run once:
+
+  pinMode(MOTOR_PWM, OUTPUT);
+  pinMode(MOTOR_UI, OUTPUT);
+  pinMode(SOL_ON_EN, OUTPUT);
+  // pinMode(SOL_DEG_EN, OUTPUT);
+  // pinMode(SOL_DEG_PWM, OUTPUT);
+  pinMode(SOL_ON_PWM, OUTPUT);
+  // for (int i = 0; i < 22; i++) {
+  //   pinMode(i, OUTPUT);
+  // }
+
+  
+  // put your setup code here, to run once:
+  // pinMode(MOTOR_PWM, OUTPUT);
 }
 
 void loop() {
-  int period = 1E6 / frequency; // Calculate the period of the pulse in microseconds
-  while(true){
-  SettingPWM();
-  digitalWrite(Solenoid, LOW); // Turn the output pin ON
-  delay(10);
-  for (int i = 0; i < pulses; i++) {
-    // Calculate the ON time and OFF time based on duty cycle and period
-    int onTime = (period * dutyCycle) / 100;
-    int offTime = period - onTime;
+  // put your main code here, to run repeatedly:
+  // digitalWrite(SOL_DEG_EN, LOW);
+  // digitalWrite(SOL_DEG_PWM, LOW);
+  delay(1000);
+  // digitalWrite(MOTOR_PWM, HIGH);
+  // digitalWrite(MOTOR_PWM, HIGH);
+  // digitalWrite(SOL_ON_EN, HIGH);
+  // digitalWrite(SOL_ON_PWM, HIGH);
 
-    // Output the pulse
-    digitalWrite(outputPin, HIGH); // Turn the output pin ON
-    delayMicroseconds(onTime);      // Wait for the ON time
-    digitalWrite(outputPin, LOW);  // Turn the output pin OFF
-    delayMicroseconds(offTime);     // Wait for the OFF time
-  }
-  digitalWrite(Solenoid, HIGH); // Turn the output pin ON
-  delay(800);
-  digitalWrite(Solenoid, LOW); // Turn the output pin ON
-  delay(800);
-  
-  // Stop generating pulses after numPulses
-  // while (true) {
-  //   delay(500); // Wait for 1 second
-  //   digitalWrite(LED, HIGH); // Turn the output pin ON
-  //   delay(500); // Wait for 1 second
-  //   digitalWrite(LED, LOW); // Turn the output pin OFF
+// PWM is 0-255, means 0 to 100% duty cycle, example 50 is 20% duty cycle (The formula is PWM/255 * 100%)
+// Durata is in ms, example 1000 is 1 second
 
-  // }  // Empty loop to halt execution
-  digitalToggle(LED);
-}
-}
-
-void SettingPWM(void)   {
-  // Serial.setTimeout(100);
-  int period = 1E6 / frequency; // Calculate the period of the pulse in microseconds
-  x=0;
-  while (comm_value != '9') {
-    if(Serial.available()>0){
-      comm_value=Serial.read();
+  for(int PWM = 50; PWM < 250; PWM += 40) {
+    for (int durata = 100; durata < 2000; durata += 200) {
+      
+      digitalWrite(SOL_ON_EN, LOW);
+      digitalWrite(SOL_ON_PWM, LOW);
+      analogWrite(MOTOR_PWM, PWM);
+      delay(durata);
+      analogWrite(MOTOR_PWM, 0);
+      digitalWrite(SOL_ON_EN, HIGH);
+      digitalWrite(SOL_ON_PWM, HIGH);
+      delay(200);
+      digitalWrite(SOL_ON_EN, LOW);
+      digitalWrite(SOL_ON_PWM, LOW);
     }
   }
 
-  while(comm_value!='2'){
-
-    Serial.write('9');
-    // Serial.write('2');
-    // Serial.write('3');
-    while(!Serial.available());
-    comm_value=Serial.read();
-    delay(10);
-  }
-  dutyCycle=0;
-  delay(40);
-
-  while(!Serial.available());
-  dutyCycle_1=Serial.read();
-  delay(10);
-  while(Serial.available()){
-    Serial.read();
-  }
-  Serial.write(dutyCycle_1);
-  delay(10);
-
-  while(!Serial.available());
-  dutyCycle_2=Serial.read();
-  delay(10);
-
-  Serial.write(dutyCycle_2);
-  delay(10);
-  dutyCycle = (dutyCycle_1-'0')*10 + dutyCycle_2-'0';
 
 
-  pulses=0;
-  while(!Serial.available());
-  pulses_1=Serial.read();
-  delay(10);
-  while(Serial.available()){
-    Serial.read();
-  }
-  Serial.write(pulses_1);
-  delay(10);
 
-  while(!Serial.available());
-  pulses_2=Serial.read();
-  delay(10);
+  // digitalWrite(MOTOR_PWM, LOW);
+  // digitalWrite(MOTOR_UI, LOW);
+  
+  // digitalWrite(SOL_ON_EN, LOW);
+  // digitalWrite(SOL_ON_PWM, LOW);
+  // delay(1000);
+  // digitalWrite(SOL_ON_PWM, HIGH);
+  // // analogWrite(SOL_PWM, 200);
+  // delay(4000);
+  // // analogWrite(SOL_PWM, 0);
+  // digitalWrite(SOL_ON_PWM, LOW);
+  // delay(1000);
+  // digitalWrite(SOL_ON_EN, LOW);
 
-  Serial.write(pulses_2);
-  delay(10);
-  pulses = ((pulses_1-'0')*10 + pulses_2-'0')*2.5E2;
+  // for (int i = 0; i < 22; i++) {
+  //   digitalWrite(i, HIGH);
+  // }
 
-  }
+  // delay(1000);
+  // for (int i = 0; i < 22; i++) {
+  // digitalWrite(i, LOW);
+  // }
+
+  while(1);
+
+
+}
+
+// put function definitions here:
+int CloseSol(int timeMs) {
+  digitalWrite(SOL_ON_EN, HIGH);
+  digitalWrite(SOL_ON_PWM, HIGH);
+  delay(timeMs);
+  digitalWrite(SOL_ON_EN, HIGH);
+  digitalWrite(SOL_ON_PWM, HIGH);
+}
