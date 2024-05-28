e9ff42ec75ebfc440da771893490c244c89727c1
EmilianoIrace <39857581+EmilianoIrace@users.noreply.github.com>
2024-05-28 22:54:01 +0200

    debug mode

--- src/main.cpp
+++ src/main.cpp
@@ -47,13 +47,26 @@
 #define MOTOR_UI 4
 #define BUTTON 22
 
-int pump_mode = 0; // 0 is swing, 1 is solo
 
-int Build_up_swing[18] = {210, 240, 274, 282, 304, 324, 340, 352, 334, 240, 318, 392, 478, 568, 658, 722, 822, 898}; // first 9 are Stimulation, Last 9 are Expression
+int pump_mode = 1; // 0 is swing, 1 is solo
+int debug_mode = 1;
+int Build_up_debug = 845;
+int PWM_debug = 70;
+
+// int pump_mode = 0; // 0 is swing, 1 is solo
+// int debug_mode =0;
+// int debug_start = 0;
+// int Build_up_debug = 0;
+// int PWM_debug = 0;
+
+//int Build_up_swing[18] = {210, 240, 274, 282, 304, 324, 340, 352, 334, 240, 318, 392, 478, 568, 658, 722, 822, 898}; // first 9 are Stimulation, Last 9 are Expression
+int Build_up_swing[18] = {200, 225, 242, 255, 265, 276, 300, 325, 350, 235, 305, 385, 490, 535, 600, 680, 760, 845}; // first 9 are Stimulation, Last 9 are Expression
 int PWM_swing[18] = {40, 46, 50, 56, 62, 68, 72, 78, 84, 36, 40, 46, 48, 54, 58, 62, 66, 70}; // first 9 are Stimulation, Last 9 are Expression
 
-int Build_up_solo[18] = {196, 254, 254, 260, 314, 300, 379, 354, 340, 238, 332, 370, 456, 532, 592, 694, 800, 854}; // first 9 are Stimulation, Last 9 are Expression
-int PWM_solo[18] = {26, 30, 32, 34, 38, 40, 42, 44, 46, 26, 28, 30, 32, 36, 38, 40, 42}; // first 9 are Stimulation, Last 9 are Expression
+//int Build_up_solo[18] = {196, 254, 254, 260, 314, 300, 379, 354, 340, 238, 332, 370, 456, 532, 592, 694, 800, 854}; // first 9 are Stimulation, Last 9 are Expression
+int Build_up_solo[18] = {250, 245, 254, 285, 290, 280, 320, 340, 360, 260, 392, 522, 682, 630, 725, 555, 710, 870}; // first 9 are Stimulation, Last 9 are Expression
+int PWM_solo[18] = {26, 30, 32, 34, 38, 40, 42, 44, 46, 26, 28, 30, 32, 36, 38, 40, 42, 46}; // first 9 are Stimulation, Last 9 are Expression
+
 
 // int Build_up_[18] = {};
 // int PWM_[18] = {};
@@ -66,7 +79,7 @@ int *PWM = PWM_swing;
 
 // put function declarations here:
 int myFunction(int, int);
-int myPWM(int, int, int);
+int myPWM(int, int, int,int);
 
 
 void setup() {
@@ -101,8 +114,41 @@ void loop() {
 // PWM is 0-255, means 0 to 100% duty cycle, example 50 is 20% duty cycle (The formula is PWM/255 * 100%)
 // Durata is in ms, example 1000 is 1 second
   int counter=0;
+  int j=0;
   delay(10);
   while(1){
+
+    if (debug_mode == 1) {
+        digitalWrite(SOL_ON_EN, HIGH);
+        for (int i = 0; i <= 10; i++) {
+        delay(100);
+        // analogWrite(MOTOR_PWM, (1.5/4*255)); // We need to apply 1.5V for 35ms
+
+        digitalWrite(SOL_ON_EN, LOW);
+        digitalWrite(SOL_ON_PWM, LOW);
+        // delay(35);
+        
+        myPWM(35, (1.5/4*255), 20, MOTOR_PWM);
+        myPWM(int((Build_up_debug-35)), PWM_debug*255/100, 20, MOTOR_PWM);
+        delay(50);
+        
+        digitalWrite(SOL_ON_EN, HIGH);
+        myPWM(400,60*255/100, 20, SOL_ON_PWM);
+        
+        digitalWrite(SOL_ON_EN, LOW);
+        digitalWrite(SOL_ON_PWM, LOW);
+        // digitalWrite(SOL_ON_EN, HIGH);
+        // digitalWrite(SOL_ON_PWM, HIGH);
+        // delay(200);
+        // digitalWrite(SOL_ON_EN, LOW);
+        // digitalWrite(SOL_ON_PWM, LOW);
+        }
+        debug_mode = 0;
+        digitalWrite(SOL_ON_EN, LOW);
+        digitalWrite(SOL_ON_PWM, LOW);
+
+    } else 
+    {
     for(int i = 0; i <= 17; i += 1) {
         while(digitalRead(BUTTON) == LOW) {
           delay(10);
@@ -136,16 +182,17 @@ void loop() {
         digitalWrite(SOL_ON_PWM, LOW);
         // delay(35);
         for (int j = -120; j <= 120; j=j+40) {
-        myPWM(35, (1.5/4*255), 20);
-        myPWM(int((Build_up[i]-35)*(1+j/1000.0)), PWM[i]*255/100, 20);
+        myPWM(35, (1.5/4*255), 20, MOTOR_PWM);
+        myPWM(int((Build_up[i]-35)*(1+j/1000.0)), PWM[i]*255/100, 20, MOTOR_PWM);
         delay(50);
         digitalWrite(SOL_ON_EN, HIGH);
         digitalWrite(SOL_ON_PWM, HIGH);
-        delay(400);
+        delay(300);
         digitalWrite(SOL_ON_EN, LOW);
         digitalWrite(SOL_ON_PWM, LOW);
         }
       } 
+    }
     }
   }
 
@@ -186,15 +233,15 @@ void loop() {
   // }
 
 
-int myPWM(int durationMs, int PWM, int frequencyKhz) {
+int myPWM(int durationMs, int PWM, int frequencyKhz,int pin) {
   int periodUs = 1E3/frequencyKhz;
   int onTime = periodUs * PWM / 255;
   int offTime = periodUs - onTime;
   int cycles = durationMs*1E3 / periodUs;
   for (int i = 0; i < cycles; i++) {
-    digitalWrite(MOTOR_PWM, HIGH);
+    digitalWrite(pin, HIGH);
     nrf_delay_us(onTime);
-    digitalWrite(MOTOR_PWM, LOW);
+    digitalWrite(pin, LOW);
     nrf_delay_us(offTime);
   }
   return 1;
